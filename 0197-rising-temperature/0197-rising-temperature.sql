102ef15e858fcb73698e2c9157823b799a094c30
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-07-12 14:17:19 +0530

    Time: 527 ms (48.69%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,5 @@
 SELECT w1.id 
 FROM Weather w1
 JOIN Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
 AND w1.temperature > w2.temperature 
