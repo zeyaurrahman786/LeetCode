5d3cae74d850dce7664ca3a6d0d9c30aa9fe85b3
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-07-23 19:01:49 +0530

    Time: 1 ms (100%), Space: 45.5 MB (25.97%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
@@ -4,19 +4,10 @@ class Solution {
         int j = -1;
         for (int i = n - 1; i >= 0; i--) {
             int d = num.charAt(i) - '0';
-            if (d % 2 == 1) {
-                j = i;
-                break;
+            if (d % 2 != 0) {
+                return num.substring(0, i+1);
             }
         }
-        if(j== -1){
-            return "";
-        }
-        String ans= "";
-        for(int i =0; i<=j;i++){
-            ans+=num.charAt(i);
-
-        }
-        return ans;
+        return "";
     }
 }
