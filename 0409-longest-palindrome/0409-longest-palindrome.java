c31eb962681cf3a3c3123ce1f43594f3706e4dd3
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-07-27 19:24:13 +0530

    Time: 4 ms (73.73%), Space: 41.6 MB (59.69%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -3,39 +3,36 @@ class Solution {
         int[] lower = new int[26];
         int[] upper = new int[26];
 
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
                 lower[s.charAt(i) - 'a']++;
-            }
-            else{
+            } else {
                 upper[s.charAt(i) - 'A']++;
 
             }
         }
-        
+
         boolean check = false;
         int ans = 0;
-        for(int i=0; i<26; i++){
-            if(lower[i]%2==0){
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] % 2 == 0) {
                 ans += lower[i];
-            }
-            else{
-                ans += (lower[i]-1);
+            } else {
+                ans += (lower[i] - 1);
                 check = true;
             }
         }
 
-        for(int i=0; i<26; i++){
-            if(upper[i]%2==0){
+        for (int i = 0; i < 26; i++) {
+            if (upper[i] % 2 == 0) {
                 ans += upper[i];
-            }
-            else{
-                ans += (upper[i]-1);
+            } else {
+                ans += (upper[i] - 1);
                 check = true;
             }
         }
-        if(check == true){
-            ans ++;
+        if (check == true) {
+            ans++;
         }
         return ans;
     }
