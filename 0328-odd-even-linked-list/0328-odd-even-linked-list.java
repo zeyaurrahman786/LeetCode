bfb79f11f5227fbf6c65bee3e4a9dfcf4e3c0656
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-08-03 12:25:22 +0530

    Time: 0 ms (100%), Space: 44.3 MB (59%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -13,18 +13,23 @@ class Solution {
         if (head == null) {
             return head;
         }
+
         ListNode odd = head;
         ListNode even = head.next;
+
         // Maintain the even head
         ListNode evenHead = even;
+
         while (even != null && even.next != null) {
             // Change pointers for odd list
             odd.next = odd.next.next;
             odd = odd.next;
+
             // Change pointers for even list
             even.next = even.next.next;
             even = even.next;
         }
+
         // Assign even list at the end of odd list
         odd.next = evenHead;
         return head;
