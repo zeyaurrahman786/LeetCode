910c026768af5c91185056a527d5d531684b4f78
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-08-07 22:15:07 +0530

    Time: 8 ms (56.81%), Space: 43.2 MB (5.01%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,46 +1,33 @@
 class Solution {
-    public String numberToWords(int num) {
-        if (num == 0)
-            return "Zero";
-        String[] bigString = new String[] { "Thousand", "Million", "Billion" };
-        String result = numberToWordsHelper(num % 1000);
-        num = num / 1000;
-        if (num > 0 && num % 1000 > 0) {
-            result = numberToWordsHelper(num % 1000) + "Thousand " + result;
-        }
-        num = num / 1000;
-        if (num > 0 && num % 1000 > 0) {
-            result = numberToWordsHelper(num % 1000) + "Million " + result;
-        }
-        num = num / 1000;
-        if (num > 0) {
-            result = numberToWordsHelper(num % 1000) + "Billion " + result;
-        }
-        return result.trim();
-    }
 
-    public String numberToWordsHelper(int num) {
-        String[] digitString = new String[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-                "Nine" };
-        String[] teenString = new String[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
-                "Seventeen", "Eighteen", "Nineteen" };
-        String[] tenString = new String[] { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
-                "Ninety" };
-        String result = "";
-        if (num > 99) {
-            result += digitString[num / 100] + " Hundred ";
+    String[] belowTen = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+    String[] belowTwenty = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+            "Eighteen", "Nineteen" };
+    String[] belowHundred = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
+            "Ninety" };
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
         }
-        num = num % 100;
-        if (num < 20 && num > 9) {
-            result += teenString[num % 10] + " ";
-        } else {
-            if (num > 19) {
-                result += tenString[num / 10] + " ";
-            }
-            num = num % 10;
-            if (num > 0)
-                result += digitString[num] + " ";
+        if(num < 10) {
+            return belowTen[num];
         }
-        return result;
+        if(num < 20) {
+            return belowTwenty[num-10];
+        }
+        if(num < 100) { // 74  70
+            return belowHundred[num/10] + (num%10 != 0 ? " " + belowTen[num%10] : "");
+        }
+        if(num < 1000) { // 900 -> 9 00
+            return belowTen[num/100] + " Hundred" + (num%100 != 0 ? " " + numberToWords(num%100) : "");
+        }
+        if(num < 1000000) { // 9000       999000 ->  999 000
+            return numberToWords(num/1000) + " Thousand" + (num%1000 != 0 ? " " + numberToWords(num%1000) : "");
+        }
+        if(num < 1000000000) { // 999129786  999 129786
+            return numberToWords(num/1000000) + " Million" + (num%1000000 != 0 ? " " + numberToWords(num%1000000) : "");
+        }
+        return numberToWords(num/1000000000) + " Billion" + (num%1000000000 != 0 ? " " + numberToWords(num%1000000000) : "");
     }
 }
