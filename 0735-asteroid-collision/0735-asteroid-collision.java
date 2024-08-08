dd85be24e2162db417561a79a8c21e132e6dbd54
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-08-08 18:51:58 +0530

    Time: 5 ms (82.51%), Space: 45.3 MB (53.59%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> s = new Stack<Integer>();
+        for (int a : asteroids) {
+            if (a > 0) {
+                s.push(a);
+            } else {
+                while (!s.isEmpty() && s.peek() > 0 && -a > s.peek()) {
+                    s.pop();
+                }
+                if (s.isEmpty() || s.peek() < 0) {
+                    s.push(a);
+                }
+                if (s.peek() == -a) {
+                    s.pop();
+                }
+            }
+
+        }
+        int[] ans = new int[s.size()];
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = s.pop();
+        }
+        return ans;
+    }
+}
