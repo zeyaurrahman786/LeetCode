e56d3791e4e550e5e12f244c543018d5282fcb7a
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-08-13 19:27:04 +0530

    Time: 8 ms (14.9%), Space: 49 MB (62.59%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,20 +1,31 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int maxSum = Integer.MIN_VALUE; // Initialize maxSum to the smallest possible integer
-        int currentMax = 0; // Variable to keep track of the current maximum subarray sum
-        int minSum = Integer.MAX_VALUE; // Initialize minSum to the largest possible integer
-        int currentMin = 0; // Variable to keep track of the current minimum subarray sum
-        int totalSum = 0; // Variable to keep track of the total sum of the array
-
-        for (int num : nums) {
-            currentMax = Math.max(currentMax + num, num);
-            maxSum = Math.max(maxSum, currentMax);
-
-            currentMin = Math.min(currentMin + num, num);
-            minSum = Math.min(minSum, currentMin);
-
-            totalSum += num;
+        int neg = 0, max = nums[0];
+        for (int i : nums) {
+            if (i < 0)
+                neg++;
+            max = Math.max(max, i);
         }
-        return maxSum > 0 ? Math.max(maxSum, totalSum - minSum) : maxSum;
+        int n = nums.length;
+        if (neg == n)
+            return max;
+        int ans1 = Kadane(nums);
+        int sum = Arrays.stream(nums).sum();
+        for (int i = 0; i < n; i++)
+            nums[i] = -nums[i];
+        int ans2 = Kadane(nums);
+        return Math.max(ans1, ans2 + sum);
+    }
+
+    // Kadane's Algorithm
+    public int Kadane(int[] nums) {
+        int sum = 0, ans = Integer.MIN_VALUE;
+        for (int i : nums) {
+            sum += i;
+            ans = Math.max(ans, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+        return ans;
     }
 }
