244d42c204dda98058249fbc282b6bc6d09141db
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-08-15 21:45:33 +0530

    Time: 61 ms (46.09%), Space: 55.6 MB (98.21%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,10 +1,3 @@
-// class Solution {
-//     public int findKthLargest(int[] nums, int k) {
-//         Arrays.sort(nums);
-//         return nums[nums.length-k];
-//     }
-// }
-
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq=new PriorityQueue<>(k+1);
@@ -19,38 +12,3 @@ class Solution {
         return pq.poll();
     }
 }
-
-// class Solution {
-//     public int findKthLargest(int[] nums, int k) {
-//         return quickselect(nums,0,nums.length-1,k);
-//     }
-    
-//     int quickselect(int[] nums,int left,int right,int k){
-//         int pivot=left;
-        
-//         for(int i=left;i<right;i++){
-//             if(nums[i]<=nums[right]){
-//                 swap(nums,pivot++,i);
-//             }
-//         }
-        
-//         swap(nums,pivot,right);
-        
-//         int count=right-pivot+1;
-        
-//         if(count==k){
-//             return nums[pivot];
-//         }
-//         else if(count>k){
-//             return quickselect(nums,pivot+1,right,k);
-//         }
-        
-//         return quickselect(nums,left,pivot-1,k-count);
-//     }
-    
-//     void swap(int[] nums,int l,int r){
-//         int temp=nums[l];
-//         nums[l]=nums[r];
-//         nums[r]=temp;
-//     }
-// }
