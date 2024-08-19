89fd37692682ca70c03df53e85447e92f94908b4
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-08-19 15:46:41 +0530

    Time: 1 ms (82.53%), Space: 41.7 MB (10.22%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         Map<Character, String> mp = new HashMap<>();
-
         String arr[] = s.split(" ");
         if (arr.length != pattern.length())
             return false;
@@ -10,17 +9,13 @@ class Solution {
             char ch = pattern.charAt(i);
 
             if (mp.containsKey(ch)) {
-                // If the pattern character is already mapped, check if it maps to the same
-                // string
                 if (!mp.get(ch).equals(arr[i])) {
                     return false;
                 }
             } else {
-                // If the string word is already mapped to a different character, return false
                 if (mp.containsValue(arr[i])) {
                     return false;
                 }
-                // Otherwise, map the pattern character to the current string word
                 mp.put(ch, arr[i]);
             }
         }
