c5b46c028af764aab4c0dd5cbfecd32f0084f9ea
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-01 23:55:30 +0530

    Time: 5 ms (54.86%), Space: 55.7 MB (20.72%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -3,13 +3,9 @@ class Solution {
         if (m * n != original.length) {
             return new int[0][0];
         }
-        int index = 0;
         int res[][] = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                res[i][j] = original[index];
-                index++;
-            }
+        for (int i = 0; i < original.length; i++) {
+            res[i / n][i % n] = original[i];
         }
         return res;
     }
