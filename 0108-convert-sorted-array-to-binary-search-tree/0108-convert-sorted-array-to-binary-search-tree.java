fb9c4e2417e209c985fe82285af8d20c3380f662
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-04 19:15:01 +0530

    Time: 0 ms (100%), Space: 43.2 MB (45.82%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -1,32 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public TreeNode add(int nums[] , int start , int end){
-        if(start > end) return null;
+    public TreeNode add(int nums[], int start, int end) {
+        if (start > end)
+            return null;
         int mid = (start + end) / 2;
         TreeNode n = new TreeNode(nums[mid]);
-        n.left = add(nums , start , mid - 1);
-        n.right = add(nums , mid + 1 , end);
+        n.left = add(nums, start, mid - 1);
+        n.right = add(nums, mid + 1, end);
         return n;
     }
+
     public TreeNode sortedArrayToBST(int[] nums) {
         TreeNode root = new TreeNode();
         root.val = nums[nums.length / 2];
-        root.left = add(nums , 0 , nums.length / 2 - 1);
-        root.right = add(nums , nums.length / 2 + 1 , nums.length - 1);
+        root.left = add(nums, 0, nums.length / 2 - 1);
+        root.right = add(nums, nums.length / 2 + 1, nums.length - 1);
         return root;
     }
 }
