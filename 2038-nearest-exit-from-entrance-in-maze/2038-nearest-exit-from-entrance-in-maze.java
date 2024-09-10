b60937c94611f154a0529af27fb907f2558bd2cd
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-10 13:54:45 +0530

    Time: 7 ms (73.13%), Space: 45.2 MB (95.41%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -1,36 +1,77 @@
 class Solution {
-    public int nearestExit(char[][] maze, int[] entrance) {
 
-        int m = maze.length;
-        int n = maze[0].length;
+    int[][] vis;
 
-        int[] dx = { 1, -1, 0, 0 };
-        int[] dy = { 0, 0, -1, 1 };
-        Deque<int[]> queue = new ArrayDeque<>();
+    public boolean isSafe(char[][] maze, int x, int y) {
+        int rowLen = maze.length;
+        int colLen = maze[0].length;
 
-        queue.add(new int[] { entrance[0], entrance[1], 0 });
-        maze[entrance[0]][entrance[1]] = '+';
-
-        while (!queue.isEmpty()) {
-            int[] curr = queue.pollFirst();
-            int r = curr[0];
-            int c = curr[1];
-            int dist = curr[2];
-
-            for (int i = 0; i < 4; i++) {
-                int x = r + dx[i];
-                int y = c + dy[i];
-
-                if (x == -1 || y == -1 || x == m || y == n)
-                    continue;
-                if (maze[x][y] == '+')
-                    continue;
-                if (x == 0 || y == 0 || x == m - 1 || y == n - 1)
-                    return dist + 1;
-                maze[x][y] = '+';
-                queue.add(new int[] { x, y, dist + 1 });
-            }
+        if (x < 0 || x >= rowLen || y < 0 || y >= colLen) {
+            return false;
         }
+
+        return vis[x][y] == 0 && maze[x][y] == '.';
+    }
+
+    public int bfs(char[][] maze, int[] entrance) {
+        int X = entrance[0];
+        int Y = entrance[1];
+
+        int x = X;
+        int y = Y;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int dis = 0;
+        q.offer(new int[] { x, y, 0 });
+        vis[x][y] = 1;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            x = curr[0];
+            y = curr[1];
+            dis = curr[2];
+
+            // up
+            if (isSafe(maze, x - 1, y)) {
+                vis[x - 1][y] = 1;
+                q.add(new int[] { x - 1, y, dis + 1 });
+            }
+
+            // down
+            if (isSafe(maze, x + 1, y)) {
+                vis[x + 1][y] = 1;
+                q.add(new int[] { x + 1, y, dis + 1 });
+            }
+
+            // left
+            if (isSafe(maze, x, y - 1)) {
+                vis[x][y - 1] = 1;
+                q.add(new int[] { x, y - 1, dis + 1 });
+            }
+
+            // right
+            if (isSafe(maze, x, y + 1)) {
+                vis[x][y + 1] = 1;
+                q.add(new int[] { x, y + 1, dis + 1 });
+            }
+
+            if (dis == 0) {
+                continue;
+            }
+
+            if (x == 0 || y == 0 || x == maze.length - 1 || y == maze[0].length - 1) {
+                return dis;
+            }
+
+        }
+
         return -1;
+    }
+
+    public int nearestExit(char[][] maze, int[] entrance) {
+        this.vis = new int[maze.length][maze[0].length];
+
+        return bfs(maze, entrance);
     }
 }
