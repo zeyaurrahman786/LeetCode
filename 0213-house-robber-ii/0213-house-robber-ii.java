555dc518230852238f3b6c7189132027e0403aa6
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-14 18:50:56 +0530

    Time: 0 ms (100%), Space: 40.7 MB (92.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,39 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length < 2)
+        int n = nums.length;
+        if (n == 1)
             return nums[0];
-
-        // Create 2 new arrays
-        int[] skipLastHouse = new int[nums.length - 1];
-        int[] skipFirstHouse = new int[nums.length - 1];
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            skipLastHouse[i] = nums[i];
-            skipFirstHouse[i] = nums[i + 1];
+        if (n == 2)
+            return Math.max(nums[0], nums[1]);
+        int a = nums[0];
+        int b = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n - 1; i++) {
+            int pick = a + nums[i];
+            int notPick = b;
+            int c = Math.max(pick, notPick);
+            a = b;
+            b = c;
         }
-
-        // Get the loot from both the possibilities
-        int lootSkippingLast = robHelper(skipLastHouse);
-        int lootSkippingFirst = robHelper(skipFirstHouse);
-
-        // Return the maximum of 2 loots
-        return Math.max(lootSkippingLast, lootSkippingFirst);
-    }
-
-    private int robHelper(int[] nums) {
-
-        if (nums.length < 2)
-            return nums[0];
-
-        int[] dp = new int[nums.length];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        int a1 = nums[1];
+        int b1 = Math.max(nums[1], nums[2]);
+        for (int i = 3; i < n; i++) {
+            int pick = a1 + nums[i];
+            int notPick = b1;
+            int c1 = Math.max(pick, notPick);
+            a1 = b1;
+            b1 = c1;
         }
-
-        return dp[nums.length - 1];
+        return Math.max(b, b1);
     }
 }
