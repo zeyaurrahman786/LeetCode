64523232bab9ce2345fcdab7949ff7cf8d471afb
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-15 18:36:27 +0530

    Time: 20 ms (75.79%), Space: 50.4 MB (85.86%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -20,38 +20,51 @@
 
 // Time Limited Exceeded
 
+// Top Down DP    -> 2D DP
 
+// class Solution {
+//     public int longestCommonSubsequence(String a, String b) {
+//         int[][] dp = new int[a.length()][b.length()];
+//         for (int i = 0; i < a.length(); i++) {
+//             for (int j = 0; j < b.length(); j++) {
+//                 dp[i][j] = -1;
+//             }
+//         }
+//         return Solve(a, b, 0, 0, dp);
+//     }
 
+//     public int Solve(String a, String b, int i, int j, int[][] dp) {
+//         if (i == a.length() || j == b.length())
+//             return 0;
+//         if (dp[i][j] != -1)
+//             return dp[i][j];
+//         int ans = 0;
+//         if (a.charAt(i) == b.charAt(j)) {
+//             ans = Solve(a, b, i + 1, j + 1, dp) + 1;
+//         } else {
+//             ans = Math.max(Solve(a, b, i + 1, j, dp), Solve(a, b, i, j + 1, dp));
+//         }
+//         dp[i][j] = ans;
+//         return dp[i][j];
+//     }
+// }
 
-
-
-
-
-// 2D Dp
+// Bottom Up DP
 
 class Solution {
     public int longestCommonSubsequence(String a, String b) {
-        int[][] dp = new int[a.length()][b.length()];
-        for (int i = 0; i < a.length(); i++){
-            for (int j = 0; j < b.length(); j++){
-                dp[i][j] = -1;
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        for (int i = a.length() - 1; i >= 0; i--) {
+            for (int j = b.length() - 1; j >= 0; j--) {
+                int ans = 0;
+                if (a.charAt(i) == b.charAt(j)) {
+                    ans = dp[i + 1][j + 1] + 1;
+                } else {
+                    ans = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+                dp[i][j] = ans;
             }
         }
-        return Solve(a, b, 0, 0, dp);
-    } 
-
-    public int Solve(String a, String b, int i, int j, int[][] dp) {
-        if (i == a.length() || j == b.length())
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];    
-        int ans = 0;
-        if (a.charAt(i) == b.charAt(j)) {
-            ans = Solve(a, b, i + 1, j + 1, dp) + 1;
-        } else {
-            ans = Math.max(Solve(a, b, i + 1, j, dp), Solve(a, b, i, j + 1, dp));
-        }
-        dp[i][j] = ans;
-        return dp[i][j];
+        return dp[0][0];
     }
 }
