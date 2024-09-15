1407d381e56a6def306dd7a9283934e78e18dc17
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-15 14:43:26 +0530

    Time: 0 ms (100%), Space: 41.2 MB (25.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,18 +3,17 @@ class Solution {
         int n = nums.length;
         if (n == 1)
             return nums[0];
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int a = nums[0];
+        int b = Math.max(nums[0], nums[1]);
         for (int i = 2; i < n; i++) {
-            int pick = dp[i - 2] + nums[i];
-            int notPick = dp[i - 1];
-            dp[i] = Math.max(pick, notPick);
+            int pick = a + nums[i];
+            int notPick = b;
+            int c = Math.max(pick, notPick);
+            a = b;
+            b = c;
         }
-        return dp[n - 1];
+        return b;
     }
 }
 
-
-// Bottom Up DP
+// Space Optimization Code
