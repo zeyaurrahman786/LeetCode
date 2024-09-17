806d28e8ba0f8bab329483bca107cc29af38eff4
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-17 19:52:25 +0530

    Time: 0 ms (100%), Space: 40.5 MB (37.75%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -1,11 +1,19 @@
 class Solution {
     public int numTilings(int n) {
-        long[] dp = new long[n + 2]; dp[0] = 1; dp[1] = 2;
-        long[] dpa = new long[n + 2]; dpa[1] = 1;
-        for (int i = 2; i < n; i ++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dpa[i - 1] * 2) % 1000000007;
-            dpa[i] = (dp[i - 2] + dpa[i - 1]) % 1000000007;
+        if (n == 1) {
+            return 1;
         }
-        return (int)dp[n - 1];
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = ((2 * dp[i - 1]) % 1000000007 + dp[i - 3]) % 1000000007;
+        }
+        return dp[n];
     }
 }
