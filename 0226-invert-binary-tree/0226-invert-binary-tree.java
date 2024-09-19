b38d69ab9c9e58b73bfc318974d6f07f6d3517c8
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-19 18:01:54 +0530

    Time: 0 ms (100%), Space: 41 MB (48.7%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -1,37 +1,31 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // Base case: end recursive call if current node is null
-        if (root == null) {
+        // base case
+        if (root == null) 
             return null;
-        }
-        // We will do a post-order traversal of the binary tree.
-        if (root.left != null) {
-            invertTree(root.left);
-        }
-        if (root.right != null) {
-            invertTree(root.right);
-        }
-
-        // Let's swap the left and right nodes at current level.
+ 
+        //Swap
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
 
+        invertTree (root.left); // Left recursive call
+        invertTree (root.right); // Right recursive call
         return root;
     }
 }
