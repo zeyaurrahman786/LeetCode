84c1d97bffcc360e21ca9ac528c148978086a885
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-20 23:17:59 +0530

    Time: 2 ms (98.63%), Space: 44.6 MB (77.38%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,13 +1,33 @@
 class Solution {
-    public String shortestPalindrome(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        String res = sb.reverse().toString();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.substring(0, s.length() - i).equals(res.substring(i))) {
-                return res.substring(0, i) + s;
+    public String shortestPalindrome(String s) {
+        int length = s.length();
+        if (length == 0) {
+            return s;
+        }
+
+        // Find the longest palindromic prefix
+        int left = 0;
+        for (int right = length - 1; right >= 0; right--) {
+            if (s.charAt(right) == s.charAt(left)) {
+                left++;
             }
         }
-        return res + s;
+
+        // If the whole string is a palindrome, return the original string
+        if (left == length) {
+            return s;
+        }
+
+        // Extract the suffix that is not part of the palindromic prefix
+        String nonPalindromeSuffix = s.substring(left);
+        StringBuilder reverseSuffix = new StringBuilder(
+                nonPalindromeSuffix).reverse();
+
+        // Form the shortest palindrome by prepending the reversed suffix
+        return reverseSuffix
+                .append(shortestPalindrome(s.substring(0, left)))
+                .append(nonPalindromeSuffix)
+                .toString();
     }
 }
