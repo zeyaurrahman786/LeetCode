cb7dacf0ff087d74d40429a8cc0baa0fc6365f19
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-21 12:51:40 +0530

    Time: 0 ms (100%), Space: 43.3 MB (26.53%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,50 +1,29 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null)
+        if(root == null){  // Base case
             return false;
-
-        // Create 2 stacks for the path and the sums
-        Stack<TreeNode> path = new Stack<>();
-        Stack<Integer> sumPath = new Stack<>();
-
-        path.push(root);
-        sumPath.push(root.val);
-
-        while (!path.isEmpty()) {
-            TreeNode temp = path.pop();
-            int tempVal = sumPath.pop();
-
-            // If a child node and we find the sum total, return true
-            if (temp.left == null && temp.right == null && tempVal == targetSum)
-                return true;
-
-            if (temp.right != null) {
-                path.push(temp.right);
-                sumPath.push(temp.right.val + tempVal);
-            }
-
-            if (temp.left != null) {
-                path.push(temp.left);
-                sumPath.push(temp.left.val + tempVal);
-            }
-
+        }
+        
+        // If it's a leaf node, check if the current sum equals the targetSum
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
         }
 
-        return false;
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val); // traverse left and right
     }
 }
