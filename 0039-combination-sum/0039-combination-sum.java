b5574bad0a6a02d8b821845ed4a6a6049c00d9d7
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-27 23:38:33 +0530

    Time: 2 ms (83.46%), Space: 44.2 MB (92.29%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, candidates, target, ans, new ArrayList<>());
-        return ans;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        ArrayList<Integer> cur = new ArrayList<Integer>();
+        recur(candidates, target, 0, cur, res);
+        return res;
     }
 
-    public void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if (ind == arr.length) {
+    public void recur(int[] candidates, int target, int ind, ArrayList<Integer> cur, List<List<Integer>> res) {
+        // base case
+
+        if (ind == candidates.length) {
             if (target == 0) {
-                ans.add(new ArrayList<>(ds));
+                res.add(new ArrayList<Integer>(cur));
             }
             return;
         }
 
-        if (arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
-            ds.remove(ds.size() - 1);
+        // pick
+
+        if (candidates[ind] <= target) {
+            cur.add(candidates[ind]);
+            recur(candidates, target - candidates[ind], ind, cur, res);
+            cur.remove(cur.size() - 1);
         }
-        findCombinations(ind + 1, arr, target, ans, ds);
+        // no pick
+        recur(candidates, target, ind + 1, cur, res);
     }
 }
