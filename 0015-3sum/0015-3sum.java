1559a328635762c7306cfeb94b21debcab789747
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-29 23:05:52 +0530

    Time: 862 ms (10.99%), Space: 52 MB (38.2%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
+        if (nums.length == 0)
+            return new ArrayList<>(res);
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
-            if (i != 0 && nums[i] == nums[i - 1])
-                continue;
-            twoSum(-nums[i], nums, i + 1, list);
-        }
-        return list;
-    }
-
-    public void twoSum(int target, int[] nums, int startIndex, List<List<Integer>> list) {
-        int i = startIndex;
-        int j = nums.length - 1;
-
-        while (i < j) {
-            if (nums[i] + nums[j] < target) {
-                i++;
-                continue;
-            }
-
-            if (nums[i] + nums[j] > target) {
-                j--;
-                continue;
-            }
-
-            list.add(Arrays.asList(-target, nums[i], nums[j]));
-            i++;
-            j--;
-
-            while (j > i && nums[j] == nums[j + 1]) {
-                j--;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[j] + nums[k];
+                if (sum == -nums[i]) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum > -nums[i])
+                    k--;
+                else if (sum < -nums[i])
+                    j++;
             }
         }
+        return new ArrayList<>(res);
     }
 }
