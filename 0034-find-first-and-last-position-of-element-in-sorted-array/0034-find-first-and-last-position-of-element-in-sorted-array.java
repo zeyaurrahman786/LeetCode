8c5d9f799165024a4aa2872a7a00798bd4b81fa7
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-09-30 12:21:43 +0530

    Time: 0 ms (100%), Space: 45.6 MB (92.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,26 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[2];
-        arr[0] = arr[1] = -1;
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
 
-        // Search for the first occurrence
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                arr[0] = i;
-                break;
+    public int findFirst(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int index = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                index = mid;  // Keep track of the first occurrence
+                high = mid - 1;  // Search the left side for earlier occurrences
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+        return index;
+    }
 
-        // If the first occurrence is found, search for the last occurrence
-        if (arr[0] != -1) {
-            for (int i = nums.length - 1; i >= 0; i--) {
-                if (nums[i] == target) {
-                    arr[1] = i;
-                    break;
-                }
+    public int findLast(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int index = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                index = mid;  // Keep track of the last occurrence
+                low = mid + 1;  // Search the right side for later occurrences
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-
-        return arr;
+        return index;
     }
 }
