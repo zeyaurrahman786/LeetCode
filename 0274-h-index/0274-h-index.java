249a8ade3f2d919a892550df568af82975df6f56
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-05 08:38:18 +0530

    Time: 2 ms (72.66%), Space: 41.7 MB (21.25%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,14 +1,11 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int h = citations.length;
-        int maxi = 0;
         Arrays.sort(citations);
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= h - i) {
-                maxi = Math.max(maxi, h - i);
-
-            }
+        int h = 0;
+        for(int i = citations.length - 1; i >= 0; i--) {
+            if(citations[i] > h)
+                h++;
         }
-        return maxi;
+        return h;
     }
 }
