13c299b766b8bd582af027b497388195ed76ebc6
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-13 19:10:39 +0530

    Time: 99 ms (63.42%), Space: 49 MB (60.73%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,24 +1,25 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n <= 2) return 0; //Checking 0 & 1
-        boolean[] composites = new boolean[n];
-        int limit = (int)Math.sqrt(n);
-        //Array of compoites --> True represent composite and False represents primes
-        for(int i = 2; i <= limit; i++){
-            if(composites[i] == false) {
-                //Mark all the multiples of i as true. 
-                //The first index to be flipped to true, is i*i
-                for(int j = i*i; j < n; j+=i){
-                    composites[j] = true;
+        if (n <= 2)
+            return 0;
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
-        
+
         int count = 0;
-        for(int i = 2; i < n; i++) {
-            if(composites[i] == false) count++;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i])
+                count++;
         }
-        
+
         return count;
     }
 }
