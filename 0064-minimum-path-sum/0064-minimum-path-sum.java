1f9a68b90dd2a7fa4bc8772d8dcc277ae8bc8fcd
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-16 19:29:33 +0530

    Time: 3 ms (69.08%), Space: 47.7 MB (20.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -13,7 +13,6 @@ class Solution {
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-
                 grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
             }
         }
