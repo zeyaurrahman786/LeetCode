132cd143ce8d19defb1319564ae7adadfba0e1ce
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-18 19:54:13 +0530

    Time: 8 ms (86%), Space: 40.5 MB (98.25%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,31 +1,24 @@
 class Solution {
+    public int maxOr = 0;
+    public int count = 0;
+
     public int countMaxOrSubsets(int[] nums) {
-        // target or
-        int targetOr = 0;
-        for (int num : nums) {
-            targetOr |= num;
-        }
-        // state: index,targetOr = 7
-        Integer dp[][] = new Integer[nums.length][targetOr + 1];
-        return recur(0, nums, 0, targetOr, dp);
+        int n = nums.length;
+
+        for (int num : nums)
+            maxOr |= num;
+        backtrack(nums, n, 0, 0);
+        return count;
     }
 
-    // tc: n*targetOr
-    // sc: n*targetOr
-    public int recur(int index, int nums[], int curOr, int targetOr, Integer dp[][]) {
-        // base case
-        if (index == nums.length) {
-            return (curOr == targetOr) ? 1 : 0;
+    public void backtrack(int[] nums, int n, int index, int currOr) {
+        if (index == n) {
+            if (currOr == maxOr) {
+                count++;
+            }
+            return;
         }
-        // check if already solved
-        if (dp[index][curOr] != null) {
-            return dp[index][curOr];
-        }
-        // O(2^n)
-        // pick
-        int pickCount = recur(index + 1, nums, curOr | nums[index], targetOr, dp);
-        // no pick
-        int noPickCount = recur(index + 1, nums, curOr, targetOr, dp);
-        return dp[index][curOr] = pickCount + noPickCount;
+        backtrack(nums, n, index + 1, currOr | nums[index]);
+        backtrack(nums, n, index + 1, currOr);
     }
 }
