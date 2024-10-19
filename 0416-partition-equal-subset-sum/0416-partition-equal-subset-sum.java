0ed4c64672d18199acc2e9d7f75fc217dd7614ed
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-19 09:14:41 +0530

    Time: 49 ms (46.5%), Space: 45.8 MB (73.69%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,29 +1,17 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int arraySum = 0;
-
-        // Calculate sum of array.
         for (int num : nums) {
             arraySum += num;
         }
-
-        // If total sum is odd, it cannot be partitioned into equal sum subsets.
         if (arraySum % 2 != 0) {
             return false;
         }
-
-        // Calculate the subset sum.
         int subsetSum = arraySum / 2;
-
-        // Create a lookup table and fill all entries with FALSE.
         boolean[][] dp = new boolean[subsetSum + 1][nums.length + 1];
-
-        // Initialize the first row as TRUE as each array has a subset whose sum is zero
         for (int i = 0; i <= nums.length; i++) {
             dp[0][i] = true;
         }
-
-        // Fill the lookup table in a bottom-up manner.
         for (int i = 1; i <= subsetSum; i++) {
             for (int j = 1; j <= nums.length; j++) {
                 if (nums[j - 1] > i) {
@@ -33,8 +21,6 @@ class Solution {
                 }
             }
         }
-
-        // Return the last index of the matrix, which is our answer.
         return dp[subsetSum][nums.length];
     }
 }
