32659447cfaecd035e97199583b7395c2ed1a71b
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-26 23:13:42 +0530

    Time: 8 ms (100%), Space: 72.1 MB (82.81%) - LeetHub

--- 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.java
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.java
@@ -1,55 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
-    int[] depth;
-    int[] dmax1;
-    int[] dmax2;
-    int[] larray;
+
+    static final int[] heights = new int[100001];
+    int maxHeight = 0;
 
     public int[] treeQueries(TreeNode root, int[] queries) {
-        // initializations:
-        depth = new int[100001];
-        dmax1 = new int[100001];
-        dmax2 = new int[100001];
-        larray = new int[100001];
-        int[] h = new int[queries.length];
-        height(root, 0);
+        getLeftHeights(root, 0);
+        maxHeight = 0;
+        getRightHeights(root, 0);
 
-        for (int i = 0; i < queries.length; i++) {
-            int q = queries[i];
-            int level = larray[q];
-            h[i] = (dmax1[level] == depth[q] ? dmax2[level] : dmax1[level]) + level - 1;
+        int n = queries.length;
+        int[] result = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = heights[queries[i]];
         }
-        return h;
+
+        return result;
     }
 
-    int height(TreeNode root, int level) {
+    private void getLeftHeights(TreeNode node, int height) {
+        heights[node.val] = maxHeight;
+        maxHeight = Math.max(maxHeight, height);
+        if (node.left != null)
+            getLeftHeights(node.left, height + 1);
+        if (node.right != null)
+            getLeftHeights(node.right, height + 1);
+    }
 
-        if (root == null)
-            return 0;
-
-        larray[root.val] = level;
-        depth[root.val] = Math.max(height(root.left, level + 1), height(root.right, level + 1)) + 1;
-        if (dmax1[level] < depth[root.val]) {
-            dmax2[level] = dmax1[level];
-            dmax1[level] = depth[root.val];
-        } else if (dmax2[level] < depth[root.val]) {
-            dmax2[level] = depth[root.val];
-        }
-
-        return depth[root.val];
+    private void getRightHeights(TreeNode node, int height) {
+        heights[node.val] = Math.max(heights[node.val], maxHeight);
+        maxHeight = Math.max(height, maxHeight);
+        if (node.right != null)
+            getRightHeights(node.right, height + 1);
+        if (node.left != null)
+            getRightHeights(node.left, height + 1);
     }
 }
