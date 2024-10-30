93c0295f66d125f01914b36e844d7f7eae94e96e
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-10-30 15:31:26 +0530

    Time: 4 ms (84.14%), Space: 44.7 MB (82.53%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,23 +1,36 @@
 class Solution {
     public String convert(String s, int numRows) {
-        String[] ans = new String[numRows];
-        for(int i = 0; i <numRows; i++){
-            ans[i] = "";
+        if (numRows == 1)
+            return s;
+
+        StringBuilder[] rows = new StringBuilder[Math.min(numRows, s.length())];
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new StringBuilder();
         }
 
-        int i = 0;
-        while(i < s.length()){
-            for(int index = 0; index < numRows && i < s.length(); index++){
-                ans[index] += s.charAt(i++);
-            }
-            for(int index = numRows - 2; index > 0 && i < s.length(); index--){
-                ans[index] += s.charAt(i++);
-            }
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+            if (curRow == 0 || curRow == numRows - 1)
+                goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
         }
-        String res = "";
-        for(String str : ans){
-            res += str;
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
         }
-        return res;
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        System.out.println(sol.convert(s, numRows));
+
     }
 }
