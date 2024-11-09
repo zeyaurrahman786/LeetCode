c37c838bb64966f1d422dd882ab58b13a1cac2a6
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-11-09 23:08:32 +0530

    Time: 1764 ms (5.36%), Space: 40.6 MB (79.81%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.java
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -1,10 +1,10 @@
 class Solution {
     public long minEnd(int n, int x) {
-        long a = x;
-
-        while (--n > 0) {
-            a = (a+1) | x;
+        long ans = x;
+        n -= 1;
+        while (n-- > 0) {
+            ans = (ans + 1) | x;
         }
-        return a;
+        return ans;
     }
 }
