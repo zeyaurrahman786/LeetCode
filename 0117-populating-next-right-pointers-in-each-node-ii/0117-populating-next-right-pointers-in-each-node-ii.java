76be5a5ba9faca05d0887893872bc85b5e53491d
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-11-12 19:57:56 +0530

    Time: 0 ms (100%), Space: 44.1 MB (52.85%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -21,30 +21,70 @@ class Node {
 };
 */
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if (root == null)
+//             return null;
+
+//         Queue<Node> q = new LinkedList<>();
+//         q.add(root);
+
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             Node dummy = new Node(0);
+//             while (size-- > 0) {
+//                 Node node = q.remove();
+//                 dummy.next = node;
+//                 dummy = dummy.next;
+
+//                 if (node.left != null) {
+//                     q.add(node.left);
+//                 }
+//                 if (node.right != null) {
+//                     q.add(node.right);
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+// }
+
+// TC -> O(N)
+// SC -> O(N)
+
+
+
+
+
+
 class Solution {
     public Node connect(Node root) {
         if (root == null)
             return null;
 
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
+        Node head = root;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
+        while (head != null) {
             Node dummy = new Node(0);
-            while (size-- > 0) {
-                Node node = q.remove();
-                dummy.next = node;
-                dummy = dummy.next;
+            Node temp = dummy;
 
-                if (node.left != null) {
-                    q.add(node.left);
+            while (head != null) {
+                if (head.left != null) {
+                    temp.next = head.left;
+                    temp = temp.next;
                 }
-                if (node.right != null) {
-                    q.add(node.right);
+                if(head.right != null) {
+                    temp.next = head.right;
+                    temp = temp.next;
                 }
+                head = head.next;
             }
+            head = dummy.next;
         }
+
         return root;
     }
 }
+
+// TC -> O(N)
+// SC -> O(1)
