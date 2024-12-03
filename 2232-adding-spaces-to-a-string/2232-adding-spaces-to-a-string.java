40267888e5434ddf39de6fdcc1592425427ac7fd
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-12-03 19:18:28 +0530

    Time: 21 ms (88.52%), Space: 78.6 MB (85.1%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
@@ -1,15 +1,15 @@
 // insert method
-class Solution {
-    public String addSpaces(String s, int[] spaces) {
-        StringBuilder sb = new StringBuilder(s);
-        int spaceCounter = 0;
-        for (int spaceIndex : spaces) {
-            sb.insert(spaceIndex + spaceCounter, " ");
-            spaceCounter++;
-        }
-        return sb.toString();
-    }
-}
+// class Solution {
+//     public String addSpaces(String s, int[] spaces) {
+//         StringBuilder sb = new StringBuilder(s);
+//         int spaceCounter = 0;
+//         for (int spaceIndex : spaces) {
+//             sb.insert(spaceIndex + spaceCounter, " ");
+//             spaceCounter++;
+//         }
+//         return sb.toString();
+//     }
+// }
 
 
 
@@ -18,19 +18,19 @@ class Solution {
 
 
 // two pointer
-// class Solution {
-//     public String addSpaces(String s, int[] spaces) {
-//         int n = s.length();
-//         int m = spaces.length;
-//         StringBuilder sb = new StringBuilder(n + m);
-//         int spaceIndex = 0;
-//         for (int stringIndex = 0; stringIndex < n; stringIndex++) {
-//             if (spaceIndex < m && spaces[spaceIndex] == stringIndex) {
-//                 sb.append(' ');
-//                 spaceIndex++;
-//             }
-//             sb.append(s.charAt(stringIndex));
-//         }
-//         return sb.toString();
-//     }
-// }
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        int n = s.length();
+        int m = spaces.length;
+        StringBuilder sb = new StringBuilder(n + m);
+        int spaceIndex = 0;
+        for (int stringIndex = 0; stringIndex < n; stringIndex++) {
+            if (spaceIndex < m && spaces[spaceIndex] == stringIndex) {
+                sb.append(' ');
+                spaceIndex++;
+            }
+            sb.append(s.charAt(stringIndex));
+        }
+        return sb.toString();
+    }
+}
