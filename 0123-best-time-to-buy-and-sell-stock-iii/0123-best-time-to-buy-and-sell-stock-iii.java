7aa1fc022320fc71c52687fdf7805a645c57cad3
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-12-08 22:45:19 +0530

    Time: 80 ms (28.81%), Space: 59.4 MB (91.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,28 +1,30 @@
 // Buy and Sell Stock Pattern
-// Approach - 2 Tabulation
+// Approach - 3 Space Optimized
 
 
 class Solution {
     public int maxProfit(int[] prices) {
         
         int n = prices.length;
-        int [][][]dp = new int [n+1][2][3];
+        int [][]prev = new int [2][3];
+        int [][]cur = new int [2][3];
         int profit =0;
         
         for(int i=n-1;i>=0;i--){
             for(int buy=0;buy<=1;buy++){
                 for(int transaction =1;transaction<=2;transaction++){
                     if(buy ==0){
-                        profit = Math.max(0+dp[i+1][0][transaction], -prices[i]+ dp[i+1][1][transaction]);
+                        profit = Math.max(0+prev[0][transaction], -prices[i]+ prev[1][transaction]);
                     }
                     if(buy ==1){
-                        profit = Math.max(0+dp[i+1][1][transaction],prices[i]+dp[i+1][0][transaction-1]);
+                        profit = Math.max(0+prev[1][transaction],prices[i]+prev[0][transaction-1]);
                     }
-                    dp[i][buy][transaction] = profit;
+                    cur[buy][transaction] = profit;
                 }
+                prev = (int [][])(cur.clone());
             }
         }
-        return dp[0][0][2];
+        return prev[0][2];
         
     }
 }
