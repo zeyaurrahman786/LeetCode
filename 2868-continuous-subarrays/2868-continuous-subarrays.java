bfd151279434b2a13ced856f59e976a77ed21910
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-12-14 23:17:26 +0530

    Time: 3 ms (94.44%), Space: 58 MB (72.77%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.java
+++ 2868-continuous-subarrays/2868-continuous-subarrays.java
@@ -1,49 +1,20 @@
 // o(NlogN)
-class Solution {
-    public long continuousSubarrays(int[] nums) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        int start = 0;
-        int end = 0;
-        long count = 0;
-        int n = nums.length;
-        while (end < n) {
-            map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
-            // shrinking
-            while (map.lastEntry().getKey() - map.firstEntry().getKey() > 2) {
-                map.put(nums[start], map.get(nums[start]) - 1);
-                if (map.get(nums[start]) == 0) {
-                    map.remove(nums[start]);
-                }
-                start++;
-            }
-            count += (end - start + 1);
-            end++;
-        }
-        return count;
-    }
-}
-
-// most optimal solution - O(N)
 // class Solution {
 //     public long continuousSubarrays(int[] nums) {
+//         TreeMap<Integer, Integer> map = new TreeMap<>();
 //         int start = 0;
 //         int end = 0;
-//         int curMin = Integer.MAX_VALUE;
-//         int curMax = Integer.MIN_VALUE;
 //         long count = 0;
 //         int n = nums.length;
 //         while (end < n) {
-//             curMin = Math.min(curMin, nums[end]);
-//             curMax = Math.max(curMax, nums[end]);
-//             if (curMax - curMin > 2) {
-//                 start = end;
-//                 curMin = nums[end];
-//                 curMax = nums[end];
-//                 while (start - 1 >= 0 && Math.abs(nums[start - 1] - nums[end]) <= 2) {
-//                     start--;
-//                     curMin = Math.min(curMin, nums[start]);
-//                     curMax = Math.max(curMax, nums[start]);
+//             map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
+//             // shrinking
+//             while (map.lastEntry().getKey() - map.firstEntry().getKey() > 2) {
+//                 map.put(nums[start], map.get(nums[start]) - 1);
+//                 if (map.get(nums[start]) == 0) {
+//                     map.remove(nums[start]);
 //                 }
+//                 start++;
 //             }
 //             count += (end - start + 1);
 //             end++;
@@ -51,3 +22,32 @@ class Solution {
 //         return count;
 //     }
 // }
+
+// most optimal solution - O(N)
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        int start = 0;
+        int end = 0;
+        int curMin = Integer.MAX_VALUE;
+        int curMax = Integer.MIN_VALUE;
+        long count = 0;
+        int n = nums.length;
+        while (end < n) {
+            curMin = Math.min(curMin, nums[end]);
+            curMax = Math.max(curMax, nums[end]);
+            if (curMax - curMin > 2) {
+                start = end;
+                curMin = nums[end];
+                curMax = nums[end];
+                while (start - 1 >= 0 && Math.abs(nums[start - 1] - nums[end]) <= 2) {
+                    start--;
+                    curMin = Math.min(curMin, nums[start]);
+                    curMax = Math.max(curMax, nums[start]);
+                }
+            }
+            count += (end - start + 1);
+            end++;
+        }
+        return count;
+    }
+}
