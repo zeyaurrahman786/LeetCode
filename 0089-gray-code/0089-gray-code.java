32ce82dd3bcfaf62ce2db365db6884824f10fe7e
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-12-14 23:26:56 +0530

    Time: 7 ms (63.79%), Space: 51.8 MB (31.4%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,33 +1,18 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        List<Integer> result = new ArrayList<>();
-        result.add(0);
-        Set<Integer> visited = new HashSet<>();
-        visited.add(0);
-
-        if (generateGrayCode(0, n, result, visited)) {
-            return result;
+       List<Integer> list = new ArrayList<Integer>();
+       list.add(0);
+       if(n==0){
+        return list;
+       }
+       list.add(1);
+       int c = 1;
+       for(int i=2;i<=n;i++){
+        c*=2;
+        for(int j=list.size()-1;j>=0;j--){
+            list.add(c+list.get(j));
         }
-        return result;
-    }
-
-    private boolean generateGrayCode(int num, int n, List<Integer> result, Set<Integer> visited) {
-        if (result.size() == (1 << n))
-            return true;
-        for (int i = 0; i < n; i++) {
-            int next = num ^ (1 << i);
-
-            if (!visited.contains(next)) {
-                result.add(next);
-                visited.add(next);
-
-                if (generateGrayCode(next, n, result, visited))
-                    return true;
-
-                result.remove(result.size() - 1);
-                visited.remove(next);
-            }
-        }
-        return false;
+       }
+       return list;
     }
 }
