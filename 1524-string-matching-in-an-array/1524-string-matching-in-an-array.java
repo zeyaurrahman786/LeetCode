3f6dab80722c55cf1dc9bbab8590dcee9056a73e
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-01-07 16:01:55 +0530

    Time: 7 ms (12.23%), Space: 42.2 MB (54.32%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,23 +1,21 @@
 // // build in method
-class Solution {
-    public List<String> stringMatching(String[] words) {
-        List<String> res = new ArrayList<>();
-        int n = words.length;
-        for (int i = 0;i < n;i++) {
+// class Solution {
+//     public List<String> stringMatching(String[] words) {
+//         List<String> res = new ArrayList<>();
+//         int n = words.length;
+//         for (int i = 0;i < n;i++) {
+//             for (int j = 0;j < n;j++) {
+//                 if (i == j || words[i].length() > words[j].length()) continue;
+//                 if (words[j].contains(words[i])) {
+//                     res.add(words[i]);
+//                     break; 
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
 
-            for (int j = 0;j < n;j++) {
-                if (i == j || words[i].length() > words[j].length()) continue;
-
-                if (words[j].contains(words[i])) {
-                    res.add(words[i]);
-                    break; 
-                }
-            }
-
-        }
-        return res;
-    }
-}
 
 
 
@@ -25,45 +23,46 @@ class Solution {
 
 
 // // custom compare method
-// class Solution {
-//     public List<String> stringMatching(String[] words) {
-//         List<String> res = new ArrayList<>();
-//         int n = words.length;
-//         for (int i = 0;i < n;i++) {
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> res = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
 
-//             for (int j = 0;j < n;j++) {
-//                 if (i == j || words[i].length() > words[j].length()) continue;
+            for (int j = 0; j < n; j++) {
+                if (i == j || words[i].length() > words[j].length())
+                    continue;
 
-//                 if (isSubstring( words[i], words[j] )) {
-//                     res.add(words[i]);
-//                     break; 
-//                 }
-//             }
+                if (isSubstring(words[i], words[j])) {
+                    res.add(words[i]);
+                    break;
+                }
+            }
 
-//         }
+        }
 
-//         return res;
-//     }
+        return res;
+    }
 
-//     public boolean isSubstring(String sub, String word) {
-//         int n = word.length();
-//         int m = sub.length();
-//         for (int j = 0; j< n; j++) {
-//             boolean isMatch = true;
-//             int k=j;
-//             int i=0;
-//             while(i<m){
-//                 if (k >= word.length() || word.charAt(k) != sub.charAt(i)) {
-//                     isMatch = false;
-//                     break; 
-//                 }
-//                 k++;
-//                 i++;
-//             }
-//             if (isMatch) {
-//                 return true; 
-//             }
-//         }
-//         return false;
-//     }
-// }
+    public boolean isSubstring(String sub, String word) {
+        int n = word.length();
+        int m = sub.length();
+        for (int j = 0; j < n; j++) {
+            boolean isMatch = true;
+            int k = j;
+            int i = 0;
+            while (i < m) {
+                if (k >= word.length() || word.charAt(k) != sub.charAt(i)) {
+                    isMatch = false;
+                    break;
+                }
+                k++;
+                i++;
+            }
+            if (isMatch) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
