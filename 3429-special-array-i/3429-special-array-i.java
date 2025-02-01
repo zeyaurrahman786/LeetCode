ebaf36238f9e6eccc72f128c2a9c023830556c6c
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-02-01 21:28:26 +0530

    Time: 0 ms (100%), Space: 43.5 MB (32.6%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public boolean isArraySpecial(int[] nums) {
+        // Iterate through indexes 0 to n - 1
+        for (int index = 0; index < nums.length - 1; index++) {
+            // Compare the parities of the current and next number
+            if (nums[index] % 2 == nums[index + 1] % 2) {
+                // If the two adjacent numbers have the same parity, return false
+                return false;
+            }
+        }
+
+        // Return true if no pair of adjacent numbers with the same parity are found
+        return true;
+    }
+}
