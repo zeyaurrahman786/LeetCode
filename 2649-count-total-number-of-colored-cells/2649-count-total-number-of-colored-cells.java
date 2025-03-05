edb0404fd15fac1c85dfd7c480a59f4b89e059e2
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-03-05 14:36:53 +0530

    Time: 0 ms (100%), Space: 40.8 MB (16.39%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,15 +1,6 @@
 class Solution {
 
     public long coloredCells(int n) {
-        long numBlueCells = 1;
-        int addend = 4;
-
-        // Iterate n - 1 times
-        while (--n > 0) {
-            // Add and update current multiple of 4
-            numBlueCells += addend;
-            addend += 4;
-        }
-        return numBlueCells;
+        return 1 + (long) n * (n - 1) * 2;
     }
 }
