2d627eb6adce62f7c86afab35df4a6b3eed09c6d
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-03-27 21:59:30 +0530

    Time: 11 ms (95.51%), Space: 56.4 MB (86.54%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,31 +1,41 @@
 class Solution {
 
     public int minimumIndex(List<Integer> nums) {
-        Map<Integer, Integer> firstMap = new HashMap<>();
-        Map<Integer, Integer> secondMap = new HashMap<>();
-        int n = nums.size();
+        // Find the majority element
+        int x = nums.get(0), count = 0, xCount = 0, n = nums.size();
 
-        // Add all elements of nums to secondMap
         for (int num : nums) {
-            secondMap.put(num, secondMap.getOrDefault(num, 0) + 1);
+            if (num == x) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                x = num;
+                count = 1;
+            }
         }
 
-        for (int index = 0; index < n; index++) {
-            // Create split at current index
-            int num = nums.get(index);
-            secondMap.put(num, secondMap.get(num) - 1);
-            firstMap.put(num, firstMap.getOrDefault(num, 0) + 1);
+        // Count frequency of majority element
+        for (int num : nums) {
+            if (num == x) {
+                xCount++;
+            }
+        }
 
-            // Check if valid split
-            if (
-                firstMap.get(num) * 2 > index + 1 &&
-                secondMap.get(num) * 2 > n - index - 1
-            ) {
+        // Check if valid split is possible
+        count = 0;
+        for (int index = 0; index < n; index++) {
+            if (nums.get(index) == x) {
+                count++;
+            }
+            int remainingCount = xCount - count;
+            // Check if both left and right partitions satisfy the condition
+            if (count * 2 > index + 1 && remainingCount * 2 > n - index - 1) {
                 return index;
             }
         }
 
-        // No valid split exists
         return -1;
     }
 }
