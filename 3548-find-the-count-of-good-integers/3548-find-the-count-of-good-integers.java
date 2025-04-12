bbd740007873fc7df4c378899a7bbaa4cbdec1f4
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-04-12 21:35:43 +0530

    Time: 403 ms (32%), Space: 47.6 MB (60%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.java
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.java
@@ -1,39 +1,40 @@
 class Solution {
-
     public long countGoodIntegers(int n, int k) {
-        Set<String> dict = new HashSet<>();
-        int base = (int) Math.pow(10, (n - 1) / 2);
-        int skip = n & 1;
-        /* Enumerate the number of palindrome numbers of n digits */
-        for (int i = base; i < base * 10; i++) {
-            String s = Integer.toString(i);
-            s += new StringBuilder(s).reverse().substring(skip);
-            long palindromicInteger = Long.parseLong(s);
-            /* If the current palindrome number is a k-palindromic integer */
-            if (palindromicInteger % k == 0) {
-                char[] chars = s.toCharArray();
-                Arrays.sort(chars);
-                dict.add(new String(chars));
-            }
+        long[] fac = new long[n + 1];
+        fac[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            fac[i] = fac[i - 1] * i;
         }
 
-        long[] factorial = new long[n + 1];
-        factorial[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial[i] = factorial[i - 1] * i;
-        }
         long ans = 0;
-        for (String s : dict) {
+        Set<String> vis = new HashSet<>();
+        int base = (int) Math.pow(10, (n - 1) / 2);
+
+        for (int i = base; i < base * 10; i++) {
+            String s = String.valueOf(i);
+            StringBuilder sb = new StringBuilder(s).reverse();
+            s += sb.substring(n % 2);
+            if (Long.parseLong(s) % k != 0) {
+                continue;
+            }
+
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String t = new String(arr);
+            if (vis.contains(t)) {
+                continue;
+            }
+            vis.add(t);
             int[] cnt = new int[10];
-            for (char c : s.toCharArray()) {
+            for (char c : arr) {
                 cnt[c - '0']++;
             }
-            /* Calculate permutations and combinations */
-            long tot = (n - cnt[0]) * factorial[n - 1];
+
+            long res = (n - cnt[0]) * fac[n - 1];
             for (int x : cnt) {
-                tot /= factorial[x];
+                res /= fac[x];
             }
-            ans += tot;
+            ans += res;
         }
 
         return ans;
