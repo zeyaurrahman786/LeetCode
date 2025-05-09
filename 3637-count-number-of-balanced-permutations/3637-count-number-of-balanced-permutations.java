8f18658f84795e26d27f6f6909196659087e0873
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-05-09 22:06:29 +0530

    Time: 61 ms (75%), Space: 45.2 MB (100%) - LeetHub

--- 3637-count-number-of-balanced-permutations/3637-count-number-of-balanced-permutations.java
+++ 3637-count-number-of-balanced-permutations/3637-count-number-of-balanced-permutations.java
@@ -1,15 +1,10 @@
 class Solution {
 
     private static final long MOD = 1_000_000_007;
-    private long[][][] memo;
-    private int[] cnt;
-    private int[] psum;
-    private int target;
-    private long[][] comb;
 
     public int countBalancedPermutations(String num) {
         int tot = 0, n = num.length();
-        cnt = new int[10];
+        int[] cnt = new int[10];
         for (char ch : num.toCharArray()) {
             int d = ch - '0';
             cnt[d]++;
@@ -19,11 +14,11 @@ class Solution {
             return 0;
         }
 
-        target = tot / 2;
+        int target = tot / 2;
         int maxOdd = (n + 1) / 2;
+        long[][] comb = new long[maxOdd + 1][maxOdd + 1];
+        long[][] f = new long[target + 1][maxOdd + 1];
 
-        /* Pre-calculate combinations */
-        comb = new long[maxOdd + 1][maxOdd + 1];
         for (int i = 0; i <= maxOdd; i++) {
             comb[i][i] = comb[i][0] = 1;
             for (int j = 1; j < i; j++) {
@@ -31,48 +26,44 @@ class Solution {
             }
         }
 
-        psum = new int[11];
-        for (int i = 9; i >= 0; i--) {
-            psum[i] = psum[i + 1] + cnt[i];
-        }
-
-        memo = new long[10][target + 1][maxOdd + 1];
-        for (long[][] arr2 : memo) {
-            for (long[] arr1 : arr2) {
-                Arrays.fill(arr1, -1);
+        f[0][0] = 1;
+        int psum = 0, totSum = 0;
+        for (int i = 0; i <= 9; i++) {
+            /* Sum of the number of the first i digits */
+            psum += cnt[i];
+            /* Sum of the first i numbers */
+            totSum += i * cnt[i];
+            for (
+                int oddCnt = Math.min(psum, maxOdd);
+                oddCnt >= Math.max(0, psum - (n - maxOdd));
+                oddCnt--
+            ) {
+                /* The number of bits that need to be filled in even numbered positions */
+                int evenCnt = psum - oddCnt;
+                for (
+                    int curr = Math.min(totSum, target);
+                    curr >= Math.max(0, totSum - target);
+                    curr--
+                ) {
+                    long res = 0;
+                    for (
+                        int j = Math.max(0, cnt[i] - evenCnt);
+                        j <= Math.min(cnt[i], oddCnt) && i * j <= curr;
+                        j++
+                    ) {
+                        /* The current digit is filled with j positions at odd positions, and cnt[i] - j positions at even positions */
+                        long ways =
+                            (comb[oddCnt][j] * comb[evenCnt][cnt[i] - j]) % MOD;
+                        res =
+                            (res +
+                                ((ways * f[curr - i * j][oddCnt - j]) % MOD)) %
+                            MOD;
+                    }
+                    f[curr][oddCnt] = res % MOD;
+                }
             }
         }
 
-        return (int) dfs(0, 0, maxOdd);
-    }
-
-    private long dfs(int pos, int curr, int oddCnt) {
-        /* If the remaining positions cannot be legally filled, or if the sum of the elements at the current odd positions is greater than the target value */
-        if (oddCnt < 0 || psum[pos] < oddCnt || curr > target) {
-            return 0;
-        }
-        if (pos > 9) {
-            return curr == target && oddCnt == 0 ? 1 : 0;
-        }
-        if (memo[pos][curr][oddCnt] != -1) {
-            return memo[pos][curr][oddCnt];
-        }
-        /* Even-numbered positions remaining to be filled */
-        int evenCnt = psum[pos] - oddCnt;
-        long res = 0;
-        for (
-            int i = Math.max(0, cnt[pos] - evenCnt);
-            i <= Math.min(cnt[pos], oddCnt);
-            i++
-        ) {
-            /* The current digit is filled with i positions at odd positions, and cnt[pos] - i positions at even positions */
-            long ways = (comb[oddCnt][i] * comb[evenCnt][cnt[pos] - i]) % MOD;
-            res =
-                (res +
-                    ((ways * dfs(pos + 1, curr + i * pos, oddCnt - i)) % MOD)) %
-                MOD;
-        }
-        memo[pos][curr][oddCnt] = res;
-        return res;
+        return (int) f[target][maxOdd];
     }
 }
