ee6bda80e0bd1abe168c8d7bd66f7744fc6f7872
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-05-14 22:51:24 +0530

    Time: 167 ms (59.46%), Space: 45.1 MB (97.3%) - LeetHub

--- 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.java
+++ 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.java
@@ -1,78 +1,75 @@
-class Solution {
+import java.util.*;
 
-    private static final int MOD = (int) 1e9 + 7;
-    private static final int L = 26;
-
-    private static class Mat {
-
-        int[][] a = new int[L][L];
-
-        Mat() {
-        }
-
-        Mat(Mat copyFrom) {
-            for (int i = 0; i < L; i++) {
-                System.arraycopy(copyFrom.a[i], 0, this.a[i], 0, L);
-            }
-        }
-
-        Mat mul(Mat other) {
-            Mat result = new Mat();
-            for (int i = 0; i < L; i++) {
-                for (int j = 0; j < L; j++) {
-                    for (int k = 0; k < L; k++) {
-                        result.a[i][j] = (int) ((result.a[i][j] +
-                                (long) this.a[i][k] * other.a[k][j]) %
-                                MOD);
-                    }
-                }
-            }
-            return result;
-        }
-    }
-
-    /* identity matrix */
-    private Mat I() {
-        Mat m = new Mat();
-        for (int i = 0; i < L; i++) {
-            m.a[i][i] = 1;
-        }
-        return m;
-    }
-
-    /* matrix exponentiation by squaring */
-    private Mat quickmul(Mat x, int y) {
-        Mat ans = I();
-        Mat cur = new Mat(x);
-        while (y > 0) {
-            if ((y & 1) == 1) {
-                ans = ans.mul(cur);
-            }
-            cur = cur.mul(cur);
-            y >>= 1;
-        }
-        return ans;
-    }
+public class Solution {
+    static final int MOD = 1_000_000_007;
 
     public int lengthAfterTransformations(String s, int t, List<Integer> nums) {
-        Mat T = new Mat();
-        for (int i = 0; i < L; i++) {
-            for (int j = 1; j <= nums.get(i); j++) {
-                T.a[(i + j) % L][i] = 1;
+        // Convert List<Integer> to int[]
+        int[] numArray = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            numArray[i] = nums.get(i);
+        }
+
+        int[][] T = getTransformationMatrix(numArray);
+        int[][] poweredT = matrixPow(T, t);
+        int[] count = new int[26];
+        int[] lengths = new int[26];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                lengths[j] = (int)((lengths[j] + 1L * count[i] * poweredT[i][j]) % MOD);
             }
         }
 
-        Mat res = quickmul(T, t);
-        int[] f = new int[L];
-        for (char ch : s.toCharArray()) {
-            f[ch - 'a']++;
+        int total = 0;
+        for (int len : lengths) {
+            total = (total + len) % MOD;
         }
-        int ans = 0;
-        for (int i = 0; i < L; i++) {
-            for (int j = 0; j < L; j++) {
-                ans = (int) ((ans + (long) res.a[i][j] * f[j]) % MOD);
+
+        return total;
+    }
+
+    private int[][] getTransformationMatrix(int[] nums) {
+        int[][] T = new int[26][26];
+        for (int i = 0; i < nums.length; i++) {
+            for (int step = 1; step <= nums[i]; step++) {
+                T[i][(i + step) % 26]++;
             }
         }
-        return ans;
+        return T;
+    }
+
+    private int[][] getIdentityMatrix(int size) {
+        int[][] I = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            I[i][i] = 1;
+        }
+        return I;
+    }
+
+    private int[][] matrixMult(int[][] A, int[][] B) {
+        int size = A.length;
+        int[][] C = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                long sum = 0;
+                for (int k = 0; k < size; k++) {
+                    sum = (sum + 1L * A[i][k] * B[k][j]) % MOD;
+                }
+                C[i][j] = (int)sum;
+            }
+        }
+        return C;
+    }
+
+    private int[][] matrixPow(int[][] M, int n) {
+        if (n == 0) return getIdentityMatrix(M.length);
+        if (n % 2 == 1) return matrixMult(M, matrixPow(M, n - 1));
+        int[][] half = matrixPow(M, n / 2);
+        return matrixMult(half, half);
     }
 }
