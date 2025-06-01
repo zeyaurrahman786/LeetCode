0dcf9cb55d742cb53e742d8dc0e8bbc40a2e6c05
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-06-01 11:07:01 +0530

    Time: 24 ms (30.88%), Space: 40.9 MB (82.35%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.java
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.java
@@ -1,18 +1,13 @@
 class Solution {
 
     public long distributeCandies(int n, int limit) {
-        return (
-            cal(n + 2) -
-            3 * cal(n - limit + 1) +
-            3 * cal(n - (limit + 1) * 2 + 2) -
-            cal(n - 3 * (limit + 1) + 2)
-        );
-    }
-
-    public long cal(int x) {
-        if (x < 0) {
-            return 0;
+        long ans = 0;
+        for (int i = 0; i <= Math.min(limit, n); i++) {
+            if (n - i > 2 * limit) {
+                continue;
+            }
+            ans += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
         }
-        return ((long) x * (x - 1)) / 2;
+        return ans;
     }
 }
