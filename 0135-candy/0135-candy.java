364968c4b887e859491e1ef18f53a97261e1919a
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-06-02 07:38:11 +0530

    Time: 2 ms (99.56%), Space: 45.4 MB (90.86%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,36 +1,17 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int totalCandies = n;
-        int i = 1;
-
-        while (i < n) {
-            if (ratings[i] == ratings[i - 1]) {
-                i++;
-                continue;
-            }
-
-            int currentPeak = 0;
-            while (i < n && ratings[i] > ratings[i - 1]) {
-                currentPeak++;
-                totalCandies += currentPeak;
-                i++;
-            }
-
-            if (i == n) {
-                return totalCandies;
-            }
-
-            int currentValley = 0;
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                currentValley++;
-                totalCandies += currentValley;
-                i++;
-            }
-
-            totalCandies -= Math.min(currentPeak, currentValley);
+        int cnt = 0;
+        int[] candies = new int[n];
+        for (int i = 0; i < n; i++) candies[i] = 1;
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
         }
-
-        return totalCandies;
+        return cnt + candies[n - 1];
     }
 }
