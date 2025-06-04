e246b2d518a4d16948cbaa3339979059e99754c4
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-06-04 12:10:40 +0530

    Time: 2 ms (92.59%), Space: 43.5 MB (99.07%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -1,17 +1,29 @@
 class Solution {
 
+    public String lastSubstring(String s) {
+        int i = 0, j = 1, n = s.length();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s.charAt(i + k) == s.charAt(j + k)) {
+                k++;
+            }
+            if (j + k < n && s.charAt(i + k) < s.charAt(j + k)) {
+                int t = i;
+                i = j;
+                j = Math.max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substring(i);
+    }
+
     public String answerString(String word, int numFriends) {
         if (numFriends == 1) {
             return word;
         }
-        int n = word.length();
-        String res = "";
-        for (int i = 0; i < n; i++) {
-            String s = word.substring(i, Math.min(i + n - numFriends + 1, n));
-            if (res.compareTo(s) <= 0) {
-                res = s;
-            }
-        }
-        return res;
+        String last = lastSubstring(word);
+        int n = word.length(), m = last.length();
+        return last.substring(0, Math.min(m, n - numFriends + 1));
     }
 }
