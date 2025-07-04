69caf3e65f82b25abab469222422edd174570ae9
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-07-04 21:53:04 +0530

    Time: 1 ms (100%), Space: 42.8 MB (95.12%) - LeetHub

--- 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.java
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.java
@@ -2,15 +2,10 @@ class Solution {
 
     public char kthCharacter(long k, int[] operations) {
         int ans = 0;
-        int t;
-        while (k != 1) {
-            t = 63 - Long.numberOfLeadingZeros(k);
-            if ((1L << t) == k) {
-                t--;
-            }
-            k = k - (1L << t);
-            if (operations[t] != 0) {
-                ans++;
+        k--;
+        for (int i = 63 - Long.numberOfLeadingZeros(k); i >= 0; i--) {
+            if (((k >> i) & 1) == 1) {
+                ans += operations[i];
             }
         }
         return (char) ('a' + (ans % 26));
