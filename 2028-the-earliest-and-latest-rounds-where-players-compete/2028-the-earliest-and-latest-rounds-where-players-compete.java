c9d3098a31716ae51e4832f88b41cbf0302b1dbe
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-07-12 14:53:25 +0530

    Time: 0 ms (100%), Space: 41.9 MB (65.63%) - LeetHub

--- 2028-the-earliest-and-latest-rounds-where-players-compete/2028-the-earliest-and-latest-rounds-where-players-compete.java
+++ 2028-the-earliest-and-latest-rounds-where-players-compete/2028-the-earliest-and-latest-rounds-where-players-compete.java
@@ -1,62 +1,54 @@
 class Solution {
-
-    private int[][][] F = new int[30][30][30];
-    private int[][][] G = new int[30][30][30];
-
-    private int[] dp(int n, int f, int s) {
-        if (F[n][f][s] != 0) {
-            return new int[] { F[n][f][s], G[n][f][s] };
+    public int[] earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
+        int p1 = Math.min(firstPlayer, secondPlayer);
+        int p2 = Math.max(firstPlayer, secondPlayer);
+        if (p1 + p2 == n + 1) {
+            // p1 and p2 compete in the first round
+            return new int[]{1, 1};
         }
-        if (f + s == n + 1) {
-            return new int[] { 1, 1 };
-        }
-        // F(n,f,s) = F(n, n + 1 - s, n + 1 - f)
-        if (f + s > n + 1) {
-            int[] res = dp(n, n + 1 - s, n + 1 - f);
-            F[n][f][s] = res[0];
-            G[n][f][s] = res[1];
-            return new int[] { F[n][f][s], G[n][f][s] };
+        if (n == 3 || n == 4) {
+            // p1 and p2 must compete in the second round (only two rounds).
+            return new int[]{2, 2};
         }
 
-        int earliest = Integer.MAX_VALUE;
-        int latest = Integer.MIN_VALUE;
-        int n_half = (n + 1) / 2;
-        if (s <= n_half) {
-            // On the left or in the middle
-            for (int i = 0; i < f; ++i) {
-                for (int j = 0; j < s - f; ++j) {
-                    int[] res = dp(n_half, i + 1, i + j + 2);
-                    earliest = Math.min(earliest, res[0]);
-                    latest = Math.max(latest, res[1]);
+        // Flip to make p1 be more closer to left than p2 to right end for convenience
+        if (p1 - 1 > n - p2) {
+            int t = n + 1 - p1;
+            p1 = n + 1 - p2;
+            p2 = t;
+        }
+
+        int m = (n + 1) / 2;
+        int min = n;
+        int max = 1;
+        if (p2 * 2 <= n + 1) {
+            // p2 is in the first half (n odd or even) or exact middle (n odd)
+            int a = p1 - 1;
+            int b = p2 - p1 - 1;
+            // i represents the number of front players in A wins
+            // j represents the number of front players in B wins
+            for (int i = 0; i <= a; i++) {
+                for (int j = 0; j <= b; j++) {
+                    int[] ret = earliestAndLatest(m, i + 1, i + j + 2);
+                    min = Math.min(min, 1 + ret[0]);
+                    max = Math.max(max, 1 + ret[1]);
                 }
             }
         } else {
-            // s on the right
-            int s_prime = n + 1 - s;
-            int mid = (n - 2 * s_prime + 1) / 2;
-            for (int i = 0; i < f; ++i) {
-                for (int j = 0; j < s_prime - f; ++j) {
-                    int[] res = dp(n_half, i + 1, i + j + mid + 2);
-                    earliest = Math.min(earliest, res[0]);
-                    latest = Math.max(latest, res[1]);
+            // p2 is in the later half (and has >= p1 distance to the end)
+            int p4 = n + 1 - p2;
+            int a = p1 - 1;
+            int b = p4 - p1 - 1;
+            // Group C are players between p4 and p2, (c+1)/2 will advance to the next round.
+            int c = p2 - p4 - 1;
+            for (int i = 0; i <= a; i++) {
+                for (int j = 0; j <= b; j++) {
+                    int[] ret = earliestAndLatest(m, i + 1, i + j + 1 + (c + 1) / 2 + 1);
+                    min = Math.min(min, 1 + ret[0]);
+                    max = Math.max(max, 1 + ret[1]);
                 }
             }
         }
-
-        F[n][f][s] = earliest + 1;
-        G[n][f][s] = latest + 1;
-        return new int[] { F[n][f][s], G[n][f][s] };
-    }
-
-    public int[] earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
-        // F(n,f,s) = F(n,s,f)
-        if (firstPlayer > secondPlayer) {
-            int temp = firstPlayer;
-            firstPlayer = secondPlayer;
-            secondPlayer = temp;
-        }
-
-        int[] res = dp(n, firstPlayer, secondPlayer);
-        return new int[] { res[0], res[1] };
+        return new int[]{min, max};
     }
 }
