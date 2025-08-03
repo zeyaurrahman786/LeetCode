a9f106ee203306b9a714e5e5a11aa77af4e0b6fa
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-08-03 20:53:04 +0530

    Time: 3 ms (74.68%), Space: 97.6 MB (59.49%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
@@ -1,59 +1,33 @@
 class Solution {
 
     public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int left = 0;
+        int right = 0;
         int n = fruits.length;
-        int[] sum = new int[n + 1];
-        int[] indices = new int[n];
-        sum[0] = 0;
-        for (int i = 0; i < n; i++) {
-            sum[i + 1] = sum[i] + fruits[i][1];
-            indices[i] = fruits[i][0];
-        }
+        int sum = 0;
         int ans = 0;
-        for (int x = 0; x <= k / 2; x++) {
-            /* move left x steps, then move right k - x steps. */
-            int y = k - 2 * x;
-            int left = startPos - x;
-            int right = startPos + y;
-            int start = lowerBound(indices, 0, n - 1, left);
-            int end = upperBound(indices, 0, n - 1, right);
-            ans = Math.max(ans, sum[end] - sum[start]);
-            /* move right x steps, then move left k - x steps. */
-            y = k - 2 * x;
-            left = startPos - y;
-            right = startPos + x;
-            start = lowerBound(indices, 0, n - 1, left);
-            end = upperBound(indices, 0, n - 1, right);
-            ans = Math.max(ans, sum[end] - sum[start]);
+        // each time fix the right boundary of the window
+        while (right < n) {
+            sum += fruits[right][1];
+            // move left boundary
+            while (left <= right && step(fruits, startPos, left, right) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            ans = Math.max(ans, sum);
+            right++;
         }
         return ans;
     }
 
-    public int lowerBound(int[] arr, int left, int right, int val) {
-        int res = right + 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] >= val) {
-                res = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return res;
-    }
-
-    public int upperBound(int[] arr, int left, int right, int val) {
-        int res = right + 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] > val) {
-                res = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return res;
+    public int step(int[][] fruits, int startPos, int left, int right) {
+        return (
+            Math.min(
+                Math.abs(startPos - fruits[right][0]),
+                Math.abs(startPos - fruits[left][0])
+            ) +
+            fruits[right][0] -
+            fruits[left][0]
+        );
     }
 }
