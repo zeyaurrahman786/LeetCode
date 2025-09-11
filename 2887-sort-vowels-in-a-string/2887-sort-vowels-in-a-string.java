7bcf42657686af49f0eb2a5ccf1ab4aa856841d9
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-09-11 10:02:27 +0530

    Time: 21 ms (86.82%), Space: 45.6 MB (96.78%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,35 +1,37 @@
 class Solution {
     // Returns true if the character is a vowel.
     boolean isVowel(Character c) {
-        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i' 
-            || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
+        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i'
+                || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
     }
-    
-    public String sortVowels(String s) {
-         ArrayList<Character> temp = new ArrayList<>();
 
-        // Store the vowels in the temporary string.
+    public String sortVowels(String s) {
+        int[] count = new int[1000];
+
+        // Store the frequencies for each character.
         for (char c : s.toCharArray()) {
             if (isVowel(c)) {
-                temp.add(c);
+                count[c - 'A']++;
             }
         }
-        
-        // Sort the temporary string characters in ascending order.
-        Collections.sort(temp);
 
+        // Sorted string having all the vowels.
+        String sortedVowel = "AEIOUaeiou";
         StringBuilder ans = new StringBuilder();
         int j = 0;
         for (int i = 0; i < s.length(); i++) {
-            // If the character is a vowel, replace it with the character in the string temp.
-            if (isVowel(s.charAt(i))) {
-                ans.append(temp.get(j));
-                j++;
-            } else {
+            if (!isVowel(s.charAt(i))) {
                 ans.append(s.charAt(i));
+            } else {
+                // Skip to the character which is having remaining count.
+                while (count[sortedVowel.charAt(j) - 'A'] == 0) {
+                    j++;
+                }
+
+                ans.append(sortedVowel.charAt(j));
+                count[sortedVowel.charAt(j) - 'A']--;
             }
         }
-        
         return ans.toString();
     }
 };
