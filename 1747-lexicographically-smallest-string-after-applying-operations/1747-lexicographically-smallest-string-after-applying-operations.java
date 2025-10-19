c1103f85e948d846ea102bf3625bfd8c768e8853
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2025-10-19 12:19:32 +0530

    Time: 72 ms (49.44%), Space: 45.8 MB (82.02%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
@@ -1,31 +1,27 @@
 class Solution {
-
     public String findLexSmallestString(String s, int a, int b) {
-        int n = s.length();
-        boolean[] vis = new boolean[n];
-        String res = s;
-        // double the length of s for convenience in extracting the rotated string t
-        s = s + s;
-        for (int i = 0; !vis[i]; i = (i + b) % n) {
-            vis[i] = true;
-            for (int j = 0; j < 10; j++) {
-                int kLimit = b % 2 == 0 ? 0 : 9;
-                for (int k = 0; k <= kLimit; k++) {
-                    // before each accumulation, re-truncate t
-                    char[] t = s.substring(i, i + n).toCharArray();
-                    for (int p = 1; p < n; p += 2) {
-                        t[p] = (char) ('0' + ((t[p] - '0' + j * a) % 10));
-                    }
-                    for (int p = 0; p < n; p += 2) {
-                        t[p] = (char) ('0' + ((t[p] - '0' + k * a) % 10));
-                    }
-                    String tStr = new String(t);
-                    if (tStr.compareTo(res) < 0) {
-                        res = tStr;
-                    }
-                }
-            }
+        Set<String> vis = new HashSet<>();
+        String smallest = s;
+        Deque<String> q = new ArrayDeque<>();
+        q.offer(s);
+        vis.add(s);
+
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+            if (cur.compareTo(smallest) < 0)
+                smallest = cur;
+
+            StringBuilder sb = new StringBuilder(cur);
+            for (int i = 1; i < sb.length(); i += 2)
+                sb.setCharAt(i, (char) ((sb.charAt(i) - '0' + a) % 10 + '0'));
+            String added = sb.toString();
+            if (vis.add(added))
+                q.offer(added);
+
+            String rotated = cur.substring(cur.length() - b) + cur.substring(0, cur.length() - b);
+            if (vis.add(rotated))
+                q.offer(rotated);
         }
-        return res;
+        return smallest;
     }
 }
